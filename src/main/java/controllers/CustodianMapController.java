b5a67223f9e201878d4cfbc04e6d8e2a9af80b7b
Dan <doates625@gmail.com>
2019-04-05 19:49:26 -0400

    Added user and timestamp fields to SanitationRequest and SanitationTable

--- src/main/java/controllers/CustodianMapController.java
+++ src/main/java/controllers/CustodianMapController.java
@@ -1,15 +1,10 @@
 package controllers;
 
 import com.jfoenix.controls.JFXButton;
-import controllers.MapController;
-import database.Database;
 import database.SanitationTable;
 import helpers.Constants;
-import helpers.SpillModel;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.Tooltip;
@@ -18,9 +13,7 @@ import javafx.scene.layout.AnchorPane;
 import map.MapDisplay;
 import models.sanitation.SanitationRequest;
 
-import java.net.URL;
 import java.util.List;
-import java.util.ResourceBundle;
 
 public class CustodianMapController extends MapController {
 
@@ -70,11 +63,6 @@ public class CustodianMapController extends MapController {
 
     public void markDone(){
         SanitationRequest selected = tblData.getSelectionModel().getSelectedItem();
-        if (selected.getUser().equals("")) {
-            selected.setUser("user_temp");
-        } else {
-            selected.setUser("");
-        }
         if (selected.getStatusObj() == SanitationRequest.Status.COMPLETE) {
             selected.setStatus(SanitationRequest.Status.INCOMPLETE);
         } else {
