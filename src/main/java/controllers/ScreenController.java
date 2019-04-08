1dc5711d6c17f26fc10403f0f86f7dbd7ab8829d
Dan <doates625@gmail.com>
2019-04-08 19:28:37 -0400

    Fully implemented and tested Prescription Request feature.

--- src/main/java/controllers/ScreenController.java
+++ src/main/java/controllers/ScreenController.java
@@ -60,7 +60,7 @@ public class ScreenController {
         this.addScreen(Constants.Routes.EDIT_POPUP, "/EditUserPopUp.fxml");
         this.addScreen(Constants.Routes.WORKSPACE, "/Workspace.fxml");
         this.addScreen(Constants.Routes.IT, "/requests/ITServiceRequest.fxml");
-        this.addScreen(Constants.Routes.PERSCRIPTION, "/requests/Perscription.fxml");
+        this.addScreen(Constants.Routes.PERSCRIPTION, "/requests/Prescription.fxml");
         this.addScreen(Constants.Routes.INTERPRETER, "/requests/Interpreter.fxml");
         this.addScreen(Constants.Routes.INTERNAL_TRANS, "/requests/InternalTrans.fxml");
         this.addScreen(Constants.Routes.GIFT_STORE, "/requests/GiftStore.fxml");
